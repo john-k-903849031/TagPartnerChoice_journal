005540a0dae2f81031720bd879e5837860be1c17
Kiara Johnson <kiara@oneotaslopes.org>
2026-09-02 12:40:38 -0400

    updating base plotter

--- analysis/plotting_files/base_plotter.R
+++ analysis/plotting_files/base_plotter.R
@@ -22,14 +22,6 @@ facet_nested_theme <- theme(
   panel.spacing.y = unit(0,"line"),
   axis.text.x = element_text(angle = 90, vjust = 0.2, hjust=0.2))
 
-make_combo_plot <- function(vt_sweep_plot, tag_mut_plot){
-  legend <- cowplot::get_legend(vt_sweep_plot+ theme(legend.direction = "horizontal",legend.justification="center" ,legend.box.just = "bottom"))
-  plot <- wrap_plots(((vt_sweep_plot+ theme(legend.position = "none")) + (tag_mut_plot+ theme(legend.position = "none"))), 
-                     legend, 
-                     heights = c(1, 0.1)) + plot_annotation(tag_levels  = list(c("a)", "b)", "")))
-  return(plot)
-}
-
 ########### binned behavior ###########
 classify_behavior <- function(df){
   small <- df %>% group_by(seed) %>% mutate(para_count = rowSums(across(`Hist_-1`:`Hist_-0.3`)), 
@@ -40,42 +32,52 @@ classify_behavior <- function(df){
                                               .default = "Coexistence"))
   return(small)
 }
-binned_behavior_plotter <- function(df, facet_prefix = ""){
-  small <- df %>% group_by(tag_perm, param, class) %>% summarize(count = n())
-  
-  plot <- small %>%
+binned_behavior_plotter <- function(df){
+  plot <- df %>%
     ggplot(aes(x=as.factor(tag_perm), fill = class, y=count)) +
     geom_col() +
-    facet_wrap( ~ paste0(facet_prefix, param)) + 
+    facet_wrap( ~ param, ncol = 3) + 
     facet_nested_theme +
-    scale_fill_manual(name="Behavior",values=c("Exclusive parasitism"="#891901",
-                                               "Exclusive mutualism"="#c5f6fc",
-                                               "Coexistence"="#66ff99",
-                                               "Symbiont extinction"="#cc9900")) + 
+    scale_fill_manual(name="Behavior",
+                      values=c("Exclusive parasitism"="#891901",
+                               "Exclusive mutualism"="#c5f6fc",
+                               "Coexistence"="#66ff99",
+                               "Symbiont extinction"="#cc9900"),
+                      labels = function(x) str_wrap(x, width = 5)) + 
     labs(x = "Tag permissiveness", y = "Replicate count") + 
     theme(panel.spacing.x = unit(0.5,"line"),
           panel.spacing.y = unit(0.5,"line"),
-          axis.text.x = element_text(angle = 45, hjust = 1, vjust=1))
+          axis.text.x = element_text(angle = 45, hjust = 1, vjust=1),
+          legend.direction = "horizontal",legend.justification="center" ,
+          legend.box.just = "bottom", legend.position = "bottom")
   return(plot)
 } 
 
-## parastab ##
-folder <- "../../data/exp_2_base_tagmut_evolving/"
-para_syms_df <- read_csv(paste0(folder,"sym_counts.dat"))
-para_syms_df <- subset(para_syms_df, update==max(update))
-para_syms_df <- para_syms_df %>% rename(param = tag_mut)
+## Experiment 2 ## 
+para_syms_df <- read_csv("../../data/exp_2_base_tagmut_evolving/sym_counts.dat") %>%
+  filter(update == max(update)) %>% rename(param = tag_mut)
 para_syms_df <- classify_behavior(para_syms_df)
-tag_mut_plot <- binned_behavior_plotter(para_syms_df %>% filter(tag_perm != "none"), "TM rate ")
 
-folder <- "../../data/exp_1_base_vt_evolving/"
-vt_syms_df <- read_csv(paste0(folder,"sym_counts.dat"))
-vt_syms_df <- subset(vt_syms_df, update==max(update))
-vt_syms_df <- vt_syms_df %>% rename(param = vt)
-vt_syms_df <- classify_behavior(vt_syms_df)
-vt_plot <- binned_behavior_plotter(vt_syms_df %>% filter(tag_perm != "none"), "VT rate ")
-
+small <- para_syms_df %>% group_by(tag_perm, param, class) %>% summarize(count = n())
+small <- rbind(small %>% filter(param == "none") %>% 
+                 slice(rep(row_number(), length(unique(small$param)) - 1)) %>%
+                 mutate(param = unique((small %>% filter(param != "none"))$param)), 
+               small %>% filter(param != "none")) %>% 
+  mutate(tag_perm = case_when(tag_perm == "none" ~ "no tags", .default = tag_perm),
+         param = factor(paste0("TM rate ", as.numeric(param) * 100, "%")))
+tag_mut_plot <- binned_behavior_plotter(small)
+# 4 x 6
 tag_mut_plot
+
+
+## Experiment 1 ## 
+vt_syms_df <- read_csv("../../data/exp_1_base_vt_evolving/sym_counts.dat") %>%
+  filter(update == max(update)) %>% rename(param = vt)
+vt_syms_df <- classify_behavior(vt_syms_df)
+
+small <- vt_syms_df %>% group_by(tag_perm, param, class) %>% summarize(count = n()) %>%
+  mutate(tag_perm = case_when(tag_perm == "none" ~ "no tags", .default = tag_perm),
+         param = factor(paste0("VT rate ", param * 100, "%")))
+vt_plot <- binned_behavior_plotter(small)
+#5 x 6
 vt_plot
-## combo! ##
-# 12 x 4in
-make_combo_plot(vt_plot, tag_mut_plot)
