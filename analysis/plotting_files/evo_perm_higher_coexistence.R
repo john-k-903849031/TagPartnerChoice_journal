ee2fde897bf41419e7163a2e1ae1188350638c53
Kiara Johnson <kiara@oneotaslopes.org>
2026-09-02 14:52:31 -0400

    evo perm plotting files

---
+++ analysis/plotting_files/evo_perm_higher_coexistence.R
@@ -0,0 +1,189 @@
+library(ggplot2) # data visualization 
+library(viridis) # data visualization 
+library(ggh4x) # data visualization 
+library(dplyr) # data manipulation
+library(tidyverse)
+library(cowplot)
+library(gridExtra)
+library(ggplotify)
+library(patchwork)
+library(ggpattern)
+
+## themes and colors ##
+options(scipen=999)
+facet_nested_theme <- theme(
+  strip.background = element_rect(fill = "white", colour = "grey", linetype="dotted", linewidth=0.2), 
+  panel.background = element_rect(fill='white', color='grey'),
+  panel.border = element_blank(),
+  panel.grid.major = element_blank(), 
+  panel.grid.minor = element_blank(),
+  legend.key = element_rect(color = "transparent"),
+  panel.spacing.x = unit(0,"line"),
+  panel.spacing.y = unit(0,"line"),
+  axis.text.x = element_text(angle = 90, vjust = 0.2, hjust=0.2))
+
+perm_color_map <- c("x <= 0" = "#440154FF",
+                    "0 < x <= 0.25" = "#414487FF", 
+                    "0.25 < x <= 0.5" = "#2A788EFF", 
+                    "0.5 < x <= 0.75" = "#22A884FF", 
+                    "0.75 < x <= 1" = "#7AD151FF",             
+                    "x > 1" = "#FDE725FF") 
+
+## classification methods ##
+classify_behavior <- function(df){
+  small <- df %>% group_by(seed) %>% mutate(para_count = rowSums(across(`Hist_-1`:`Hist_-0.3`)), 
+                                            mut_count = rowSums(across(Hist_0.2:Hist_0.9)))
+  small <- small %>% mutate(class = case_when(count == 0 ~ "Symbiont extinction",
+                                              ((para_count > 0) & (count - para_count) == 0) ~ "Exclusive parasitism",
+                                              ((mut_count > 0) & (count - mut_count) == 0) ~ "Exclusive mutualism",
+                                              .default = "Coexistence"))
+  return(small)
+}
+classify_permissivenss <- function(df){
+  small <- df %>% group_by(seed,evo_mut,param) %>%
+    filter(symbiont_tag_richness > 0) %>%
+    group_by(seed,evo_mut,param) %>%
+    filter(update==max(update))
+  small <- small %>% group_by(seed) %>% mutate(class = case_when(mean_host_perm <= 0 ~ "x <= 0",
+                                                                 mean_host_perm <= 0.25 & mean_host_perm > 0 ~ "0 < x <= 0.25",
+                                                                 mean_host_perm <= 0.5 & mean_host_perm > 0.25 ~ "0.25 < x <= 0.5",
+                                                                 mean_host_perm <= 0.75 & mean_host_perm > 0.5 ~ "0.5 < x <= 0.75",
+                                                                 mean_host_perm <= 1 & mean_host_perm > 0.75 ~ "0.75 < x <= 1",
+                                                                 mean_host_perm > 1 ~ "x > 1", .default = "ERROR"))
+  small$class <- factor(small$class, levels=c("x <= 0", 
+                                              "0 < x <= 0.25", 
+                                              "0.25 < x <= 0.5", 
+                                              "0.5 < x <= 0.75", 
+                                              "0.75 < x <= 1",             
+                                              "x > 1" ))
+  return(small)
+}
+
+
+
+####### interaction values ####### 
+## parastab ##
+folder <- "../../data/exp_6_evoperm_tagmut/"
+para_syms_df <- read_csv(paste0(folder,"sym_counts.dat")) %>% 
+  filter(update == max(update)) %>% rename(param = tag_mut) %>% mutate(param_name = "tag_mut")
+para_syms_df <- classify_behavior(para_syms_df)
+
+## vt ## 
+folder <- "../../data/exp_5_evoperm_vt/"
+vt_syms_df <- read_csv(paste0(folder,"sym_counts.dat")) %>% 
+  filter(update == max(update)) %>% rename(param = vt) %>% mutate(param_name = "vt")
+vt_syms_df <- classify_behavior(vt_syms_df)
+
+## combo ## 
+combo_syms_df <- rbind(vt_syms_df, para_syms_df) %>% select(param_name, param, evo_mut, seed, class) %>% rename(behavior_class = class)
+
+####### tags ####### 
+## parastab ## 
+folder <- "../../data/exp_6_evoperm_tagmut/"
+para_tags_df <- read_csv(paste0(folder,"tag_dists.dat")) %>% 
+  rename(param = tag_mut) %>% mutate(param_name = "tag_mut")
+para_tags_df <- classify_permissivenss(para_tags_df)
+
+## vt ## 
+folder <- "../../data/exp_5_evoperm_vt/"
+vt_tags_df <- read_csv(paste0(folder,"tag_dists.dat")) %>% 
+  rename(param = vt) %>% mutate(param_name = "vt")
+vt_tags_df <- classify_permissivenss(vt_tags_df)
+
+## combo ## 
+combo_tags_df <- rbind(para_tags_df, vt_tags_df) %>% select(param_name, param, evo_mut, seed, class)
+
+
+##### combined behavior and tags ##### 
+combo_df <- combo_syms_df %>% group_by(param_name, param, evo_mut, seed) %>% 
+  left_join(combo_tags_df) %>%
+  group_by(param_name, param, evo_mut, class, behavior_class) %>%
+  summarize(count = n()) %>% 
+  ungroup() %>%
+  group_by(param_name, param, evo_mut, behavior_class) %>%
+  mutate(behavior_class_count = sum(count), evo_mut = as.factor(evo_mut))
+
+
+
+test <- combo_df %>%
+  filter((param_name == "vt" & param == 0.5) | (param_name == "tag_mut" & param == 0.0005)) %>%
+  mutate(evo_mut = as.numeric(as.character(evo_mut)),
+         category = case_when(((param_name == "vt" & evo_mut <= 0.001) | 
+                                 (param_name == "tag_mut" & evo_mut < 0.001)) ~ "lower", 
+                              ((param_name == "vt" & evo_mut > 0.001) | 
+                                 (param_name == "tag_mut" & evo_mut >= 0.001 & evo_mut <= 0.005)) ~ "higher",
+                              .default = "other"),
+         param_label = case_when(param_name == "vt" ~ "VT rate 50%",
+                                 param_name == "tag_mut" ~ "TM rate 0.05%",
+                                 .default="other")) %>%
+  filter(category != "other") 
+
+View(
+  test %>% 
+    group_by(category) %>% mutate(category_count = sum(count)) %>%
+    group_by(category, behavior_class,category_count) %>% 
+    summarize(behavior_count = sum(count)) %>%
+    mutate(percent = round(behavior_count/category_count, digits = 4)*100)
+)
+
+View(
+  test %>% 
+    filter((category=="lower" & behavior_class == "Exclusive mutualism") |
+             (category=="higher" & behavior_class == "Coexistence"))%>%
+    group_by(category, behavior_class) %>% mutate(category_count = sum(count)) %>%
+    group_by(category, behavior_class, class,category_count) %>% 
+    summarize(perm_count = sum(count)) %>%
+    mutate(percent = round(perm_count/category_count, digits = 4)*100)
+)
+
+##################### plot version 1
+a <- test %>% 
+  mutate(facet_label = case_when(category == "lower"~"Lower permissiveness\nmutation",
+                                 category == "higher"~"Higher permissiveness\nmutation",
+                                 .default = "other"),
+         facet_label = factor(facet_label, levels = c("Lower permissiveness\nmutation", "Higher permissiveness\nmutation", "other"))) %>%
+  group_by(facet_label, param_label, behavior_class) %>% summarize(total = sum(count))%>%
+  ggplot(aes(x=param_label, fill = behavior_class, y=total)) +
+  geom_col() +
+  facet_wrap( ~ as.factor(facet_label)) + 
+  facet_nested_theme +
+  scale_fill_manual(name="Behavior",values=c("Exclusive parasitism"="#891901",
+                                             "Exclusive mutualism"="#c5f6fc",
+                                             "Coexistence"="#66ff99",
+                                             "Symbiont extinction"="#cc9900")) + 
+  labs(x = "", y = "Replicate count") + 
+  theme(panel.spacing.x = unit(0.5,"line"),
+        panel.spacing.y = unit(0.5,"line"),
+        axis.text.x = element_text(angle = 12.5, hjust = 0.5, vjust=0.5))+ 
+  theme(legend.direction = "horizontal",legend.justification="center" ,
+        legend.box.just = "bottom", legend.position = "bottom",
+        legend.margin=margin(0,0,0,0),
+        legend.box.margin=margin(-10,-10,-10,-10)) + 
+  guides(fill = guide_legend(nrow = 2))
+
+b <- test %>% 
+  mutate(facet_label = case_when((category == "lower" & behavior_class == "Exclusive mutualism")~"Lower\npermissiveness mutation\nand exclusive mutualism",
+                                 (category == "higher" & behavior_class == "Coexistence")~"Higher\npermissiveness mutation\nand coexistence",
+                                 .default = "other"),
+         facet_label = factor(facet_label, levels = c("Lower\npermissiveness mutation\nand exclusive mutualism", "Higher\npermissiveness mutation\nand coexistence", "other"))) %>%
+  filter(facet_label!= "other") %>%
+  group_by(facet_label, param_label, class) %>% summarize(total = sum(count))%>%
+  ggplot(aes(x = param_label, y = total, fill = class)) +
+  geom_col() + 
+  scale_fill_manual(name="Mean final\npermissiveness", values = perm_color_map) + 
+  labs(x = "", 
+       y = "Replicate count") +
+  facet_wrap(~as.factor(facet_label)) +
+  facet_nested_theme +
+  theme(panel.spacing.x = unit(0.5,"line"),
+        panel.spacing.y = unit(0.5,"line"),
+        axis.text.x = element_text(angle = 12.5, hjust = 0.5, vjust=0.5))+ 
+  theme(legend.direction = "horizontal",legend.justification="center" ,
+        legend.box.just = "bottom", legend.position = "bottom",
+        legend.margin=margin(0,0,0,0),
+        legend.box.margin=margin(-10,-10,-10,-10)) + 
+  guides(fill = guide_legend(nrow = 2))
+
+b
+
+a / b+ plot_annotation(tag_levels  = list(c("a)", "b)")))
